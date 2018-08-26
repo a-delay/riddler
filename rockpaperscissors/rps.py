499c5980cc4c366573488ae3c75b458eecdf364c
Adela <adelay243@gmail.com>
2018-08-26 03:42:08 -0400

    initial commit
    riddler - rps 8/25/2018

---
+++ rockpaperscissors/rps.py
@@ -0,0 +1,46 @@
+import random
+
+def simulate_hoops(left, right, hoops):
+    time = 0
+    while left != hoops-1 or right != 0:
+        #print("left: " + str(left))
+        #print("right: " + str(right))
+        new_left = (left + right) // 2
+        new_right = new_left if (left + right) % 2 == 0 else new_left + 1
+        time += new_left - left
+
+        games, winner = simulate_rps()
+        #print("games: " + str(games))
+        #print("winner is left: " + str(winner))
+        time += games
+        if winner: #if left won
+            if left == hoops-2:
+                time += 1
+                break
+            left = new_left
+            right = hoops-1
+        else: #if right won
+            if right == 1:
+                time += 1
+                break
+            left = 0
+            right = new_right
+    return time
+
+# return number of games and True if left won
+def simulate_rps():
+    # 1/3 time tie, 1/3 time left wins, 1/3 time right wins
+    n = random.randint(0,2)
+    if n == 0:
+        return 1, True
+    if n == 1:
+        return 1, False
+    if n == 2:
+        time, winner = simulate_rps()
+        return time + 1, winner
+
+total_time = 0
+n = 1000000
+for i in range(n):
+    total_time += simulate_hoops(0, 7, 8)
+print(total_time/n)
